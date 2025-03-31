e7d489689383b9cefeb17de039bb0cdda62e42ec
nhp <ricardoryn1317@gmail.com>
2025-03-31 09:10:29 +0800

    <leader>fF和<leader>fR改为lazyvim默认配置

--- lua/plugins/fzf.lua
+++ lua/plugins/fzf.lua
@@ -2,12 +2,12 @@
 if vim.g.vscode then
   return {}
 else
-  return {
-    "ibhagwan/fzf-lua",
-    keys = {
-      { "<leader>fF", "<cmd>FzfLua files cwd=%:p:h<cr>", desc = "Find files (cwd)" },
-      { "<leader>fR", "<cmd>FzfLua oldfiles cwd=%:p:h<cr>", desc = "Recent (cwd)" },
-      { "<leader>fC", "<cmd>FzfLua commands<cr>", desc = "Find commands" },
-    },
-  }
+  return {}
+  -- return {
+  --   "ibhagwan/fzf-lua",
+  --   keys = {
+  --     { "<leader>fF", "<cmd>FzfLua files cwd=%:p:h<cr>", desc = "Find files (cwd)" },
+  --     { "<leader>fR", "<cmd>FzfLua oldfiles cwd=%:p:h<cr>", desc = "Recent (cwd)" },
+  --   },
+  -- }
 end
