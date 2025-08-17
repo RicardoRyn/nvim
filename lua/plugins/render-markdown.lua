337c5d55f17706b0d350e10c642d9d10970e33dd
Ricardo_T8 <ricardoryn1317@gmail.com>
2025-08-17 17:00:49 +0800

    feat(markdown): 渲染markdown文件

---
+++ lua/plugins/render-markdown.lua
@@ -0,0 +1,24 @@
+return {
+  "MeanderingProgrammer/render-markdown.nvim",
+  cond = function()
+    return not vim.g.vscode
+  end,
+  ft = { "markdown", "norg", "rmd", "org", "codecompanion" },
+  dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.nvim" }, -- if you use the mini.nvim suite
+  -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
+  -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+  ---@module 'render-markdown'
+  ---@type render.md.UserConfig
+  opts = {
+    code = {
+      sign = false,
+      width = "block",
+      right_pad = 1,
+      border = "thin",
+    },
+    heading = {
+      sign = false,
+      icons = {},
+    },
+  },
+}
