93a0e738c002d70be21b3fc079ec4a2f39874ee4
Ricardo_T8 <ricardoryn1317@gmail.com>
2025-08-17 16:59:31 +0800

    fix(noice): 修改confirm框遮挡按键框
    部分confirm需要后续操作，但是该框会一直存在，遮挡后续按键输入反馈。
    现在将cmdline置于屏幕中央，不会再被confirm框遮挡。

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -6,8 +6,9 @@ return {
   event = "VeryLazy",
   dependencies = {
     "MunifTanjim/nui.nvim",
-    "rcarriga/nvim-notify", -- 太挡视野了吧，但是可以<leader>nd
+    -- "rcarriga/nvim-notify", -- 太挡视野了吧，但是可以<leader>nd
   },
+
   opts = {
     lsp = {
       override = {
@@ -31,42 +32,36 @@ return {
     },
     presets = {
       bottom_search = false,
-      command_palette = true,
+      command_palette = false, -- 为true时，confirm相关的后续信息会被遮挡
       long_message_to_split = true,
+      inc_rename = true,
       lsp_doc_border = true,
     },
     commands = {
-      history = {
-        view = "popup",
-      },
-      last = {
-        view = "popup",
-      },
-      error = {
-        view = "popup",
-      },
-      all = {
-        view = "popup",
-      },
+      history = { view = "popup" },
+      last = { view = "popup" },
+      error = { view = "popup" },
+      all = { view = "popup" },
     },
   },
-    -- stylua: ignore
-    keys = {
-      { "<leader>nm", "<cmd>messages<CR>", desc = "Messages" },
-      { "<leader>nl", function() require("noice").cmd("last") end, desc = "Last Message" },
-      { "<leader>nh", function() require("noice").cmd("history") end, desc = "History" },
-      { "<leader>ne", function() require("noice").cmd("error") end, desc = "Error" },
-      { "<leader>na", function() require("noice").cmd("all") end, desc = "All" },
-      { "<leader>nd", function() require("noice").cmd("dismiss") end, desc = "Dismiss" },
-      { "<leader>nt", function() require("noice").cmd("pick") end, desc = "Noice Picker (Telescope)" },
-      { "<c-f>", function() if not require("noice.lsp").scroll(4) then return "<c-f>" end end, silent = true, expr = true, desc = "Scroll Forward", mode = {"i", "n", "s"} },
-      { "<c-b>", function() if not require("noice.lsp").scroll(-4) then return "<c-b>" end end, silent = true, expr = true, desc = "Scroll Backward", mode = {"i", "n", "s"}},
-    },
-    -- stylua: ignore
-    config = function(_, opts)
-      if vim.o.filetype == "lazy" then
-        vim.cmd([[messages clear]])
-      end
-      require("noice").setup(opts)
-    end,
+
+  -- stylua: ignore
+  keys = {
+    { "<leader>nm", "<cmd>messages<CR>", desc = "Messages" },
+    { "<leader>nl", function() require("noice").cmd("last") end, desc = "Last Message" },
+    { "<leader>nh", function() require("noice").cmd("history") end, desc = "History" },
+    { "<leader>ne", function() require("noice").cmd("error") end, desc = "Error" },
+    { "<leader>na", function() require("noice").cmd("all") end, desc = "All" },
+    { "<leader>nd", function() require("noice").cmd("dismiss") end, desc = "Dismiss" },
+    { "<leader>nt", function() require("noice").cmd("pick") end, desc = "Noice Picker (Telescope)" },
+    { "<c-f>", function() if not require("noice.lsp").scroll(4) then return "<c-f>" end end, silent = true, expr = true, desc = "Scroll Forward", mode = {"i", "n", "s"} },
+    { "<c-b>", function() if not require("noice.lsp").scroll(-4) then return "<c-b>" end end, silent = true, expr = true, desc = "Scroll Backward", mode = {"i", "n", "s"}},
+  },
+  -- -- stylua: ignore
+  -- config = function(_, opts)
+  --   if vim.o.filetype == "lazy" then
+  --     vim.cmd([[messages clear]])
+  --   end
+  --   require("noice").setup(opts)
+  -- end,
 }
