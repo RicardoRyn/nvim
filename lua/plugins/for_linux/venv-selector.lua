c5bf645dab7f2bb30838972fc42b3019bf29a04d
Ricardo_MI <ricardoryn1317@gmail.com>
2025-09-04 23:32:24 +0800

    style(venv-selector): 显式添加CWD的解释器

--- lua/plugins/for_linux/venv-selector.lua
+++ lua/plugins/for_linux/venv-selector.lua
@@ -5,7 +5,7 @@ return {
   end,
   event = "VeryLazy",
   dependencies = {
-    -- "neovim/nvim-lspconfig",
+    "neovim/nvim-lspconfig",
     -- "mfussenegger/nvim-dap",
     -- "mfussenegger/nvim-dap-python", --optional
     -- { "nvim-telescope/telescope.nvim", branch = "0.1.x", dependencies = { "nvim-lua/plenary.nvim" } },
@@ -20,6 +20,7 @@ return {
         command = "fd /python$ ~/miniforge3/envs --max-depth 3 --full-path --color never -E /proc",
         type = "anaconda", -- 这里必须是 anaconda，才能生效
       },
+      cwd = { command = "fd '/bin/python$' $CWD --full-path --color never -E /proc -I -a -L" },
     },
   },
 }
