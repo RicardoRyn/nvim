d36c22b730ca5c796ae706bfac52d070dc1de8d8
Ricardo_T8 <ricardoryn1317@gmail.com>
2025-09-13 01:20:16 +0800

    refactor(lualine): 精简lualine的配置

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -2,103 +2,90 @@ return {
   "nvim-lualine/lualine.nvim",
   cond = not vim.g.vscode,
   event = "VeryLazy",
-  init = function()
-    vim.g.lualine_laststatus = vim.o.laststatus
-    if vim.fn.argc(-1) > 0 then
-      -- set an empty statusline till lualine loads
-      vim.o.statusline = " "
-    else
-      -- hide the statusline on the starter page
-      vim.o.laststatus = 0
-    end
-  end,
-  opts = function()
-    vim.o.laststatus = vim.g.lualine_laststatus
-
-    local opts = {
-      options = {
-        theme = "auto",
-        globalstatus = vim.o.laststatus == 3,
-        disabled_filetypes = { statusline = { "dashboard", "alpha", "ministarter", "snacks_dashboard" } },
-      },
-      sections = {
-        lualine_a = { "mode" },
-        lualine_b = { "branch" },
-        lualine_c = {
-          {
-            "diagnostics",
-            symbols = {
-              error = require("utils.icons").diagnostics.error .. " ",
-              warn = require("utils.icons").diagnostics.warn .. " ",
-              info = require("utils.icons").diagnostics.info .. " ",
-              hint = require("utils.icons").diagnostics.hint .. " ",
-            },
+  opts = {
+    sections = {
+      lualine_a = { "mode" },
+      lualine_b = { "branch" },
+      lualine_c = {
+        {
+          "diagnostics",
+          symbols = {
+            error = require("utils.icons").diagnostics.error .. " ",
+            warn = require("utils.icons").diagnostics.warn .. " ",
+            info = require("utils.icons").diagnostics.info .. " ",
+            hint = require("utils.icons").diagnostics.hint .. " ",
           },
-          { "filetype", icon_only = true, separator = "", padding = { left = 1, right = 0 } },
+        },
+        { "filetype", icon_only = true, separator = "", padding = { left = 1, right = 0 } },
             -- stylua: ignore
-            function() return vim.fn.fnamemodify(vim.fn.expand("%:p:h"), ":~") end,
-        },
-        lualine_x = {
-          -- stylua: ignore
-          {
-            function() return require("noice").api.status.command.get() end,
-            cond = function() return package.loaded["noice"] and require("noice").api.status.command.has() end,
-          },
-          -- stylua: ignore
-          {
-            function() return require("noice").api.status.mode.get() end,
-            cond = function() return package.loaded["noice"] and require("noice").api.status.mode.has() end,
-            color = { fg = "#8839ef", gui = "bold" },
-          },
-          -- stylua: ignore
-          {
-            function() return "  " .. require("dap").status() end,
-            cond = function() return package.loaded["dap"] and require("dap").status() ~= "" end,
-            color = { fg = "#ff0000", gui = "bold" },
-          },
-          -- stylua: ignore
-          {
-            require("lazy.status").updates,
-            cond = require("lazy.status").has_updates,
-            color = { fg = "#f38ba8", gui = "bold" },
-          },
-          {
-            "diff",
-            symbols = {
-              added = require("utils.icons").git.added .. "",
-              modified = require("utils.icons").git.modified .. "",
-              removed = require("utils.icons").git.deleted .. "",
-            },
-            source = function()
-              local gitsigns = vim.b.gitsigns_status_dict
-              if gitsigns then
-                return {
-                  added = gitsigns.added,
-                  modified = gitsigns.changed,
-                  removed = gitsigns.removed,
-                }
-              end
-            end,
-          },
-        },
-        lualine_y = {
-          { "progress", separator = " ", padding = { left = 1, right = 0 } },
-          { "location", padding = { left = 0, right = 1 } },
-        },
-        lualine_z = {
-          "encoding",
+            function() return vim.fn.fnamemodify(vim.fn.expand("%:p"), ":~") end,
+      },
+      lualine_x = {
+        {
           function()
-            local ok, kernels = pcall(require, "molten.status")
-            if ok and kernels.kernels then
-              return kernels.kernels()
+            return require("noice").api.status.command.get()
+          end,
+          cond = function()
+            return package.loaded["noice"] and require("noice").api.status.command.has()
+          end,
+        },
+        {
+          function()
+            return require("noice").api.status.mode.get()
+          end,
+          cond = function()
+            return package.loaded["noice"] and require("noice").api.status.mode.has()
+          end,
+          color = { fg = "#8839ef", gui = "bold" },
+        },
+        {
+          function()
+            return "  " .. require("dap").status()
+          end,
+          cond = function()
+            return package.loaded["dap"] and require("dap").status() ~= ""
+          end,
+          color = { fg = "#ff0000", gui = "bold" },
+        },
+        {
+          require("lazy.status").updates,
+          cond = require("lazy.status").has_updates,
+          color = { fg = "#f38ba8", gui = "bold" },
+        },
+        {
+          "diff",
+          symbols = {
+            added = require("utils.icons").git.added .. "",
+            modified = require("utils.icons").git.modified .. "",
+            removed = require("utils.icons").git.deleted .. "",
+          },
+          source = function()
+            local gitsigns = vim.b.gitsigns_status_dict
+            if gitsigns then
+              return {
+                added = gitsigns.added,
+                modified = gitsigns.changed,
+                removed = gitsigns.removed,
+              }
             end
-            return ""
           end,
         },
       },
-      extensions = { "neo-tree", "lazy", "fzf", "avante" },
-    }
-
-    return opts
-  end,
+      lualine_y = {
+        { "progress", separator = " ", padding = { left = 1, right = 0 } },
+        { "location", padding = { left = 0, right = 1 } },
+      },
+      lualine_z = {
+        "encoding",
+        function()
+          local ok, kernels = pcall(require, "molten.status")
+          if ok and kernels.kernels then
+            return kernels.kernels()
+          end
+          return ""
+        end,
+      },
+    },
+    extensions = { "neo-tree", "lazy", "fzf", "avante" },
+  },
 }
