210daea8ae989835bf2ab1a69e5e92f3d77a3c26
Ricardo_T8 <ricardoryn1317@gmail.com>
2025-10-14 16:26:45 +0800

    chore: restore the change

--- init.lua
+++ init.lua
@@ -6,9 +6,9 @@ require("config.keymaps")
 if not vim.g.vscode then
   -- LSP
   require("config.lsp")
-  require("config.lsp.lua_ls") -- lua
-  require("config.lsp.pyright") -- python
-  require("config.lsp.marksman") -- markdown
-  require("config.lsp.bashls") -- bash
+  require("config.lsp.lua_ls")        -- lua
+  require("config.lsp.pyright")       -- python
+  require("config.lsp.marksman")      -- markdown
+  require("config.lsp.bashls")        -- bash
   require("config.lsp.rust-analyzer") -- rust
 end
