776ab4cbab1c73a60946efa73745beb18e2a691a
Ricardo_T8 <ricardoryn1317@gmail.com>
2025-10-24 09:37:00 +0800

    style(message): show message with mini but not notify

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -21,6 +21,9 @@ return {
       error = { view = "popup" },
       all = { view = "popup" },
     },
+    routes = {
+      { filter = { event = "msg_show", }, view = "mini", },
+    },
   },
 
   -- stylua: ignore
