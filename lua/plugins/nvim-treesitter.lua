562c8e831585261d16e5066c9723a2726094e15d
Ricardo <ricardoryn1317@gmail.com>
2025-10-27 19:20:32 +0800

    fix(treesitter): fix the parse err

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -1,39 +1,12 @@
--- -- FIX: main分支的nvim-treesitter无法自动加载
--- return {
---   "nvim-treesitter/nvim-treesitter",
---   cond = not vim.g.vscode,
---   lazy = false,
---   branch = "main",
---   build = ":TSUpdate",
---   config = function()
---     local wanted = {
---       "lua",
---       "python",
---       "bash",
---       "rust",
---       "json",
---       "markdown",
---       "markdown_inline",
---       "latex",
---       "html",
---       "yaml",
---     }
---     require("nvim-treesitter").setup({})
---     require("nvim-treesitter").install(wanted)
---
---     vim.api.nvim_create_autocmd("FileType", {
---       pattern = wanted,
---       callback = function() vim.treesitter.start() end, }) end,
--- }
-
+-- 需要：cargo install --locked tree-sitter-cli
 return {
   "nvim-treesitter/nvim-treesitter",
   cond = not vim.g.vscode,
-  main = "nvim-treesitter.configs",
-  branch = "master", -- 详见本系列的附录
-  event = "VeryLazy",
-  opts = {
-    ensure_installed = {
+  lazy = false,
+  branch = "main",
+  build = ":TSUpdate",
+  config = function()
+    local languages = {
       "lua",
       "python",
       "bash",
@@ -41,12 +14,15 @@ return {
       "json",
       "markdown",
       "markdown_inline",
-      -- "latex",
+      "latex",
       "html",
       "yaml",
-    },
-    highlight = { enable = true },
-    indent = { enable = true },
-    fold = { enable = true },
-  },
+    }
+    require("nvim-treesitter").setup({})
+    require("nvim-treesitter").install(languages)
+
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = languages,
+      callback = function() vim.treesitter.start() end, })
+    end,
 }
