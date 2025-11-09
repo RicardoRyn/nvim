6be3c2b3381d1073cb176dbc13b35b8247783d3b
Ricardo_T8 <ricardoryn1317@gmail.com>
2025-11-09 12:03:30 +0800

    feat(plugins): add luasnip

---
+++ lua/plugins/luasnip.lua
@@ -0,0 +1,7 @@
+return {
+  "L3MON4D3/LuaSnip",
+  cond = not vim.g.vscode,
+  event = "VeryLazy",
+  -- follow latest release.
+  version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+}
