fd3812bad3a6778337b8c9fddab9bfba400113a1
Ricardo_T8 <ricardoryn1317@gmail.com>
2025-12-11 16:52:04 +0800

    chore(motions): reorganize flash plugin configuration

--- lua/plugins/motions.lua
+++ lua/plugins/motions.lua
@@ -8,28 +8,128 @@ return {
         prefix = { { " FlashSearch: ", "FlashPromptIcon" } },
       },
     },
-    -- stylua: ignore
-    keys = {
-      { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
-      { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
-      { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
-      { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
-      { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
-      { "xw", mode = { "n", "v" },
-        function()
-          require("flash").jump({
-            pattern = vim.fn.expand("<cword>"),
-          })
-        end, desc = "Words"
-      },
-      { "xr", mode = { "n", "v" },
-        function()
-          require("flash").jump({continue = true})
-        end, desc = "Resume"
-      },
+  -- stylua: ignore
+  keys = {
+    { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+    { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+    { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+    { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+    { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+    { "xw", mode = { "n", "v" },
+      function()
+        require("flash").jump({
+          pattern = vim.fn.expand("<cword>"),
+        })
+      end, desc = "Words"
     },
+    { "xr", mode = { "n", "v" },
+      function()
+        require("flash").jump({continue = true})
+      end, desc = "Resume"
+    },
+  },
     config = function(_, opts)
       require("flash").setup(opts)
+      -- vim.keymap.set({ "n", "v" }, "xj", function()
+      --   require("flash").jump({
+      --     search = { forward = true, wrap = false, multi_window = false, mode = "search", max_length = 0 },
+      --     label = { after = { 0, 0 } },
+      --     pattern = "^",
+      --   })
+      -- end, { desc = "Go to line below" })
+      -- vim.keymap.set({ "n", "v" }, "xk", function()
+      --   require("flash").jump({
+      --     search = { forward = false, wrap = false, multi_window = false, mode = "search", max_length = 0 },
+      --     label = { after = { 0, 0 } },
+      --     pattern = "^",
+      --   })
+      -- end, { desc = "Go to line above" })
+      -- vim.keymap.set({ "n", "v" }, "xl", function()
+      --   local Flash = require("flash")
+      --   ---@param opts Flash.Format
+      --   local function format(opts)
+      --     -- always show first and second label
+      --     return {
+      --       { opts.match.label1, "FlashMatch" },
+      --       { opts.match.label2, "FlashLabel" },
+      --     }
+      --   end
+      --   Flash.jump({
+      --     search = { mode = "search", forward = true, wrap = false, multi_window = false },
+      --     label = { after = false, before = { 0, 0 }, uppercase = false, format = format },
+      --     pattern = [[\<]],
+      --     action = function(match, state)
+      --       state:hide()
+      --       Flash.jump({
+      --         search = { max_length = 0 },
+      --         highlight = { matches = false },
+      --         label = { format = format },
+      --         matcher = function(win)
+      --           -- limit matches to the current label
+      --           return vim.tbl_filter(function(m)
+      --             return m.label == match.label and m.win == win
+      --           end, state.results)
+      --         end,
+      --         labeler = function(matches)
+      --           for _, m in ipairs(matches) do
+      --             m.label = m.label2 -- use the second label
+      --           end
+      --         end,
+      --       })
+      --     end,
+      --     labeler = function(matches, state)
+      --       local labels = state:labels()
+      --       for m, match in ipairs(matches) do
+      --         match.label1 = labels[math.floor((m - 1) / #labels) + 1]
+      --         match.label2 = labels[(m - 1) % #labels + 1]
+      --         match.label = match.label1
+      --       end
+      --     end,
+      --   })
+      -- end, { desc = "Go to next any begining of words" })
+      -- vim.keymap.set({ "n", "v" }, "xh", function()
+      --   local Flash = require("flash")
+      --   ---@param opts Flash.Format
+      --   local function format(opts)
+      --     -- always show first and second label
+      --     return {
+      --       { opts.match.label1, "FlashMatch" },
+      --       { opts.match.label2, "FlashLabel" },
+      --     }
+      --   end
+      --   Flash.jump({
+      --     search = { mode = "search", forward = false, wrap = false, multi_window = false },
+      --     label = { after = false, before = { 0, 0 }, uppercase = false, format = format },
+      --     pattern = [[\<]],
+      --     action = function(match, state)
+      --       state:hide()
+      --       Flash.jump({
+      --         search = { max_length = 0 },
+      --         highlight = { matches = false },
+      --         label = { format = format },
+      --         matcher = function(win)
+      --           -- limit matches to the current label
+      --           return vim.tbl_filter(function(m)
+      --             return m.label == match.label and m.win == win
+      --           end, state.results)
+      --         end,
+      --         labeler = function(matches)
+      --           for _, m in ipairs(matches) do
+      --             m.label = m.label2 -- use the second label
+      --           end
+      --         end,
+      --       })
+      --     end,
+      --     labeler = function(matches, state)
+      --       local labels = state:labels()
+      --       for m, match in ipairs(matches) do
+      --         match.label1 = labels[math.floor((m - 1) / #labels) + 1]
+      --         match.label2 = labels[(m - 1) % #labels + 1]
+      --         match.label = match.label1
+      --       end
+      --     end,
+      --   })
+      -- end, { desc = "Go to previous any begining of words" })
       vim.api.nvim_set_hl(0, "FlashLabel", { fg = "#ff007c", bold = true })
     end,
   },
@@ -43,23 +143,18 @@ return {
       local directions = require("hop.hint").HintDirection
       local positions = require("hop.hint").HintPosition
       hop.setup({}) -- 初始化插件
-
       vim.keymap.set({ "n", "v" }, "xj", function()
         hop.hint_lines({ direction = directions.AFTER_CURSOR })
       end, { desc = "Go to line below" })
-
       vim.keymap.set({ "n", "v" }, "xk", function()
         hop.hint_lines({ direction = directions.BEFORE_CURSOR })
       end, { desc = "Go to line above" })
-
       vim.keymap.set({ "n", "v" }, "xl", function()
         hop.hint_camel_case({ direction = directions.AFTER_CURSOR })
       end, { desc = "Go to next any begining of words" })
-
       vim.keymap.set({ "n", "v" }, "xh", function()
         hop.hint_camel_case({ direction = directions.BEFORE_CURSOR })
       end, { desc = "Go to previous any begining of words" })
-
       -- vim.keymap.set({ "n", "v" }, "<leader><leader>w", function()
       --   hop.hint_words({ direction = directions.AFTER_CURSOR, hint_position = positions.END })
       -- end, { desc = "Go to next any begining of words" })
