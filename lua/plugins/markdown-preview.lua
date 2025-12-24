721523b8007eae5639418caeafe5b37a5418533e
Ricardo_T8 <ricardoryn1317@gmail.com>
2025-12-24 09:08:06 +0800

    feat(nvim): add markdown-preview plugin with npm build

---
+++ lua/plugins/markdown-preview.lua
@@ -0,0 +1,17 @@
+return {
+  "iamcco/markdown-preview.nvim",
+  cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+  build = "try { cd app; npm install }", -- nushell 写法
+  init = function()
+    vim.g.mkdp_filetypes = { "markdown" }
+  end,
+  ft = { "markdown" },
+  keys = {
+    {
+      "<leader>um",
+      ft = "markdown",
+      "<cmd>MarkdownPreviewToggle<cr>",
+      desc = "Markdown Preview",
+    },
+  },
+}
