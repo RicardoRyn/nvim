cccc157303aa39cf268be76b08d0007211fa8f9b
Ricardo <ricardoryn1317@gmail.com>
2025-12-30 16:02:44 +0800

    chore(deps): update plugin versions and improve venv-selector config

--- lua/plugins/venv-selector.lua
+++ lua/plugins/venv-selector.lua
@@ -1,26 +1,26 @@
 return {
   "linux-cultist/venv-selector.nvim",
-  cond = function()
-    return vim.loop.os_uname().sysname == "Windows_NT" and not vim.g.vscode
-  end,
-  event = "VeryLazy",
-  dependencies = {
-    "neovim/nvim-lspconfig",
-    -- "mfussenegger/nvim-dap",
-    -- "mfussenegger/nvim-dap-python", --optional
-    -- { "nvim-telescope/telescope.nvim", branch = "0.1.x", dependencies = { "nvim-lua/plenary.nvim" } },
-  },
+  cond = not vim.g.vscode,
+  ft = "python",
+  dependencies = { "neovim/nvim-lspconfig" },
   branch = "main", -- This is the regexp branch, use this for the new version
   keys = {
     { "<leader>lv", "<cmd>VenvSelect<cr>", desc = "Virtual Env" },
   },
-  opts = {
-    search = {
-      anaconda_base = {
-        command = "fd python.exe$ E:/python/envs --max-depth 2 --full-path --color never -HI -a -L",
-        type = "anaconda", -- 这里必须是 anaconda，才能生效
+  opts = function()
+    local is_windows = vim.loop.os_uname().sysname == "Windows_NT"
+    return {
+      search = {
+        anaconda_base = {
+          command = is_windows and "fd python.exe$ E:/python/envs --max-depth 2 --full-path --color never -HI -a -L"
+            or "fd /python$ ~/miniforge3/envs --max-depth 3 --full-path --color never -E /proc",
+          type = "anaconda",
+        },
+        cwd = {
+          command = is_windows and "fd python.exe$ $CWD/.venv/Scripts --full-path --color never -HI -a -L"
+            or "fd '/bin/python$' $CWD --full-path --color never -E /proc -I -a -L",
+        },
       },
-      cwd = { command = "fd python.exe$ $CWD/.venv/Scripts --full-path --color never -HI -a -L" },
-    },
-  },
+    }
+  end,
 }
