fff28391f33055ec99eb234e9ffcf64c2ff3b034
Ricardo_T8 <ricardoryn1317@gmail.com>
2025-12-31 17:20:48 +0800

    fix(plugins): fix venv-selector notification override

--- lua/plugins/core/venv-selector.lua
+++ lua/plugins/core/venv-selector.lua
@@ -7,17 +7,28 @@ return {
   keys = {
     { "<leader>lv", "<cmd>VenvSelect<cr>", desc = "Virtual Env" },
   },
-  opts = {
-    search = {
-      anaconda_base = {
-        command = SYSTEM.is_win and "fd python.exe$ E:/python/envs --max-depth 2 --full-path --color never -HI -a -L"
-          or "fd /python$ ~/miniforge3/envs --max-depth 3 --full-path --color never -E /proc",
-        type = "anaconda",
+  config = function()
+    -- Store original vim.notify before venv-selector loads
+    local original_notify = vim.notify
+
+    require("venv-selector").setup({
+      search = {
+        anaconda_base = {
+          command = SYSTEM.is_win and "fd python.exe$ E:/python/envs --max-depth 2 --full-path --color never -HI -a -L"
+            or "fd /python$ ~/miniforge3/envs --max-depth 3 --full-path --color never -E /proc",
+          type = "anaconda",
+        },
+        cwd = {
+          command = SYSTEM.is_win and "fd python.exe$ $CWD/.venv/Scripts --full-path --color never -HI -a -L"
+            or "fd '/bin/python$' $CWD --full-path --color never -E /proc -I -a -L",
+        },
       },
-      cwd = {
-        command = SYSTEM.is_win and "fd python.exe$ $CWD/.venv/Scripts --full-path --color never -HI -a -L"
-          or "fd '/bin/python$' $CWD --full-path --color never -E /proc -I -a -L",
-      },
-    },
-  },
+    })
+    -- Restore vim.notify after venv-selector setup (it overrides in init.lua)
+    vim.schedule(function()
+      if type(vim.notify) ~= "function" and type(original_notify) == "function" then
+        vim.notify = original_notify
+      end
+    end)
+  end,
 }
