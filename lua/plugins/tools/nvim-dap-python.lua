ef8ea624637a445e1bbf84159ca653d5b68fa75e
Ricardo <ricardoryn1317@gmail.com>
2026-01-07 09:43:06 +0800

    fix(nvim-dap-python): update setup to use uv for Python debugging

--- lua/plugins/tools/nvim-dap-python.lua
+++ lua/plugins/tools/nvim-dap-python.lua
@@ -1,12 +1,14 @@
 return {
   "mfussenegger/nvim-dap-python",
+  cond = not vim.g.vscode,
   dependencies = {
     "mfussenegger/nvim-dap",
     "rcarriga/nvim-dap-ui",
   },
   ft = "python",
   config = function()
-    local path = not SYSTEM.is_win and "~/.local/share/nvim/mason/packages/debugpy/venv/bin/python" or nil
+    local path = not SYSTEM.is_win and "~/.local/share/nvim/mason/packages/debugpy/venv/bin/python"
+      or "C:/Users/RicardoRyn/AppData/Local/nvim-data/mason/packages/debugpy/venv/Scripts/python.exe"
     require("dap-python").setup(path)
   end,
 }
