b5e50ddf27cc993055b8a34bb9d127c3e6ae6f83
Ricardo_T8 <ricardoryn1317@gmail.com>
2026-01-08 08:30:03 +0800

    chore(vcs): rename vscode-diff plugin to codediff

--- lua/plugins/vcs/codediff.lua
+++ lua/plugins/vcs/codediff.lua
@@ -1,7 +1,6 @@
 -- FIX: merge中有bug，会导致增加的部分消失
 return {
-  "esmuellert/vscode-diff.nvim",
-  enabled = false,
+  "esmuellert/codediff.nvim",
   cond = not vim.g.vscode,
   dependencies = { "MunifTanjim/nui.nvim" },
   branch = "next",
