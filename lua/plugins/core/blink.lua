00fb3405fe2b039237f269e69a43ac625fe487a3
Ricardo_T8 <ricardoryn1317@gmail.com>
2026-01-18 15:35:31 +0800

    refactor(blink): simplify tab keymap configuration

--- lua/plugins/core/blink.lua
+++ lua/plugins/core/blink.lua
@@ -15,17 +15,7 @@ return {
       }
     },
     keymap = {
-      -- ['<Tab>'] = { 'accept', 'fallback' },
-      ["<Tab>"] = {
-        "snippet_forward",
-        function() -- sidekick next edit suggestion
-          return require("sidekick").nes_jump_or_apply()
-        end,
-        function() -- if you are using Neovim's native inline completions
-          return vim.lsp.inline_completion.get()
-        end,
-        "fallback",
-      },
+      ['<Tab>'] = { 'accept', 'fallback' },
       ['<CR>'] = { 'accept', 'fallback' },
       ['<C-y>'] = false,
     },
