4b474778c0ca55f769dffc22013cb00cb69d5f1c
Ricardo_T8 <ricardoryn1317@gmail.com>
2026-01-23 16:47:24 +0800

    refactor(lsp): simplify lua_ls configuration by removing Windows-specific root_dir override

--- lsp/lua_ls.lua
+++ lsp/lua_ls.lua
@@ -1,26 +1,4 @@
 local lsp_name = "lua_ls"
 local default_config = dofile(vim.fn.stdpath("data") .. "/lazy/nvim-lspconfig/lsp/" .. lsp_name .. ".lua")
-if SYSTEM.is_win then
-  local custom_config = {
-    -- HACK: 不写下面一段，会加载大量文件，耗尽内存
-    root_dir = function(bufnr, on_dir)
-      local fname = vim.api.nvim_buf_get_name(bufnr)
-      local root = require("lspconfig.util").root_pattern(
-        ".git",
-        "init.lua",
-        ".luarc.json",
-        ".luarc.jsonc",
-        ".luacheckrc",
-        ".stylua.toml",
-        "stylua.toml",
-        "selene.toml",
-        "selene.yml"
-      )(fname) or vim.fn.getcwd()
-      on_dir(root)
-    end,
-  }
-  local final_config = vim.tbl_deep_extend("force", default_config, custom_config)
-  return final_config
-end
 
 return default_config
