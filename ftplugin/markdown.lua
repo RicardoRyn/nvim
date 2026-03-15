4487fcf3f2bda0ce01131e33b8a14538779492d3
Ricardo_T8 <ricardoryn1317@gmail.com>
2026-03-15 16:31:27 +0800

    chore(markdown): disable spell checking for Markdown files

--- ftplugin/markdown.lua
+++ ftplugin/markdown.lua
@@ -1,4 +1,4 @@
 vim.opt_local.wrap = true
 vim.opt_local.linebreak = true
-vim.opt_local.spell = true  -- Markdown 开启拼写检查
+vim.opt_local.spell = false  -- Markdown 不开启拼写检查，否则中文下面全是波浪线
 vim.opt_local.conceallevel = 2  -- 隐藏 Markdown 标记（如 **bold**
