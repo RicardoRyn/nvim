91be2f3bf1f4f592f568d3e5709d083b7cef7359
Ricardo_T8 <ricardoryn1317@gmail.com>
2026-04-08 10:56:53 +0800

    refactor(init): reorder module loading and remove unused ipynb.nvim

--- init.lua
+++ init.lua
@@ -2,11 +2,11 @@ if not _G.SYSTEM then
   _G.SYSTEM = require("utils.system")
 end
 
-require("utils.debug")
-require("config.options")
 require("config.lazy")
+require("config.options")
 require("config.keymaps")
 require("config.autocmds")
+require("utils.debug")
 
 if not vim.g.vscode then
   require("config.lsp")
