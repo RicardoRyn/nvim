f0b35415a603cf3b3fd999cc0316491cfaa43777
nhp <ricardoryn1317@gmail.com>
2026-04-27 10:53:23 +0800

    feat(plugins): add code-preview.nvim and update plugin versions

---
+++ lua/plugins/tools/code-preview.lua
@@ -0,0 +1,32 @@
+return {
+  "Cannon07/code-preview.nvim",
+  cond = not vim.g.vscode,
+  event = "VeryLazy",
+  config = function()
+    require("code-preview").setup({
+      diff = {
+        layout = "tab",
+      },
+      highlights = {
+        current = { -- CURRENT (original) side — tab/vsplit layouts
+          DiffAdd = { bg = "#d0e2d1" },
+          DiffDelete = { bg = "#eac8d3" },
+          DiffChange = { bg = "#e0e7f5" },
+          DiffText = { bg ="#b0c7f5" },
+        },
+        proposed = { -- PROPOSED side — tab/vsplit layouts
+          DiffAdd = { bg = "#d0e2d1" },
+          DiffDelete = { bg = "#eac8d3" },
+          DiffChange = { bg = "#e0e7f5" },
+          DiffText = { bg = "#b0c7f5" },
+        },
+        inline = { -- inline layout
+          added = { bg = "#d0e2d1" }, -- added line background
+          removed = { bg = "#eac8d3" }, -- removed line background
+          added_text = { bg = "#e0e7f5" }, -- changed characters (added)
+          removed_text = { bg = "#e0e7f5" }, -- changed characters (removed)
+        },
+      },
+    })
+  end,
+}
